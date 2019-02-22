e9ca526d289464b2c39d746944da4ac6e2be3b42
learncodekillaz <learncodekillaz@gmail.com>
2019-02-22 11:15:50 -0800

    Preparing to add event component

--- app/javascript/components/pages/ChoicesTable.js
+++ app/javascript/components/pages/ChoicesTable.js
@@ -136,60 +136,7 @@ class ChoicesTable extends Component {
     console.log("Test", submittedCards)
 
     return(
-
       <div>
-        {events.length > 0 &&
-          <div>
-            <h1>Your current events</h1>
-            <ul>
-              {events.map((event, index) => {
-                return (
-                  <li key={index}>{event.event_name}
-                    <ul>
-                      {event.choices.map((choice,index) =>{
-                        return(
-                          <li key={index}>{choice.choice_name}</li>
-                        )
-                      })}
-
-                    </ul>
-                  </li>
-                )
-              })}
-            </ul>
-        </div>
-      }
-      { invitations.length > 0 &&
-        <div>
-        <h1>Your current invitations</h1>
-        <ul>
-          {invitations.map((invitation, index) => {
-            return (
-              <li key={index}>{invitation.event_name}
-
-              </li>
-
-            )
-          })}
-        </ul>
-        </div>
-      }
-        <div>
-          <form>
-            <label>
-              Event Name:
-              <input
-                type="text"
-                name="name"
-              />
-            </label>
-            <input
-              type="submit"
-              value="Submit"
-            />
-          </form>
-        </div>
-
         <h1>Choice</h1>
         <div className="card-list">
           <ChoiceCard moviePairs = {moviePairs} choiceSubmitted = {this.choiceSubmitted} cancelChoice = {this.cancelChoice} />
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -5,9 +5,57 @@ class Home extends Component {
     render() {
       return(
         <div>
-        <h1>Welcome to 521 App</h1>
-        
+          {events.length > 0 &&
+            <div>
+              <h1>Your current events</h1>
+              <ul>
+                {events.map((event, index) => {
+                  return (
+                    <li key={index}>{event.event_name}
+                      <ul>
+                        {event.choices.map((choice,index) =>{
+                          return(
+                            <li key={index}>{choice.choice_name}</li>
+                          )
+                        })}
 
+                      </ul>
+                    </li>
+                  )
+                })}
+              </ul>
+          </div>
+        }
+        { invitations.length > 0 &&
+          <div>
+          <h1>Your current invitations</h1>
+          <ul>
+            {invitations.map((invitation, index) => {
+              return (
+                <li key={index}>{invitation.event_name}
+
+                </li>
+
+              )
+            })}
+          </ul>
+          </div>
+        }
+          <div>
+            <form>
+              <label>
+                Event Name:
+                <input
+                  type="text"
+                  name="name"
+                />
+              </label>
+              <input
+                type="submit"
+                value="Submit"
+              />
+            </form>
+          </div>
         </div>
       )
     }
