6365e20485ffcff2a0589f1538b0f5b99a06e2fe
learncodekillaz <learncodekillaz@gmail.com>
2019-02-22 16:07:24 -0800

    CSS

--- app/javascript/components/UnauthenticatedApp.js
+++ app/javascript/components/UnauthenticatedApp.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import { Button, Nav, NavItem, NavLink, Navbar} from 'reactstrap'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import home_screen from '../images/home_screen.png'
+import movie from '../images/movie.jpeg'
+import { Parallax, Background } from 'react-parallax';
 
 import Home from './pages/Home'
 import Invitations from './pages/Invitations'
@@ -13,9 +15,18 @@ class UnauthenticatedApp extends React.Component {
   render(){
     return (
       <div>
-        <h1>Welcome to F21 Website</h1>
-        <img src={home_screen} />
-        <h2>PLEASE SIGN IN</h2>
+      <Parallax
+            bgImage={require('../images/movie.jpeg')}
+            bgImageAlt="the dog"
+            // strength={-200}
+        >
+
+        <h1 className = "unauthenticated-header">Welcome to F21 Website</h1>
+        <h2 className = "unauthenticated-header">
+          <a className = "unauthenticated-header" href="/users/sign_in">Sign In</a>
+        </h2>
+        <div style={{ height: '55vh' }} />
+        </Parallax>
       </div>
     );
   }
