afe9ef423234e9566f466f90939ed426e0e65b3b
Doug <deguirlinger@gmail.com>
2019-02-24 15:22:07 -0800

    Added default value to event.five_choices

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,10 @@ class Event < ApplicationRecord
   # validates_associated :choices
   belongs_to :invitee, class_name: "User"
   accepts_nested_attributes_for :choices
+  before_create :default_values
+
+  private
+  def default_values
+    self.five_choices = true
+  end
 end
