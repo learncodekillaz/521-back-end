b191ff51b3b921bf512d79428928defc9f7d3f20
Doug <deguirlinger@gmail.com>
2019-02-25 09:35:20 -0800

    getting started with model logic.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,20 @@ class Event < ApplicationRecord
   # validates_associated :choices
   belongs_to :invitee, class_name: "User"
   accepts_nested_attributes_for :choices
+  extend ActiveModel::Callbacks
+  # define_model_callbacks :create, :update
+  # def create
+  #   run_callbacks :create do
+  #
+  #   end
+  # end
+  # Specifies that the method below is to be executed before creation of new record.
   before_create :default_values
-
+  # Is it necessary to include "extend ActiveModel::Callbacks" with new version of Rails? Seems to work fine without it.
   private
+  # Sets five_choices value to true upon creation.
   def default_values
     self.five_choices = true
+    self.current_stage = "five_choices"
   end
 end
