c59feff242c23d3d6b15ca09d74df0f6aac48511
learncodekillaz <learncodekillaz@gmail.com>
2019-02-25 16:00:25 -0800

    Modifying Events Controller. Testing.

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Events", type: :request do
+describe "Event", type: :request do
+  let(:user){User.create(email: "hello1@gmail.com", password:"hello1")}
+  let(:event){Event.create inviter: user, invitee: user}
+  it "Updates an event" do
+    update_params = {event:{current_stage:"two_choices"}}
+    headers = {
+      "ACCEPT" => "application/json"
+    }
+    sign_in user
+    put "/events/#{event.id}", params: update_params, headers: headers
+    event.reload
+    expect(event.current_stage).to eq "two_choices"
+  end
 end
